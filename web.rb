489918d7d849595a98aef726f6aa6bd551e95565
Marcin <marcin@opensoftware.pl>
2020-02-18 13:26:24 +0100

    Update api

--- web.rb
+++ web.rb
@@ -12,23 +12,31 @@ class Web < Roda
 
   route do |r|
     r.root do
-      db_client[:meta].find.to_a
+      r.redirect('api/files')
     end
 
-    r.post 'new' do
-      service = ::Services::NewRecordService.new(db_client)
-      hashlink = service.call(r.params)
+    r.on 'api' do
+      r.on 'files' do
+        r.get String do |hashlink|
+          service = ::Services::GetRecordService.new(db_client)
+          record, file = service.call(hashlink)
 
-      hashlink
-    end
+          response.headers['Content-Disposition'] =
+            "attachment; filename=\"#{record[:filename]}.#{record[:filetype]}\""
+          file.read
+        end
 
-    r.get String do |hashlink|
-      service = ::Services::GetRecordService.new(db_client)
-      record, file = service.call(hashlink)
+        r.post do
+          service = ::Services::NewRecordService.new(db_client)
+          hashlink = service.call(r.params)
 
-      response.headers['Content-Disposition'] =
-        "attachment; filename=\"#{record[:filename]}.#{record[:filetype]}\""
-      file.read
+          hashlink
+        end
+
+        r.is do
+          db_client[:meta].find.to_a
+        end
+      end
     end
   end
 end
